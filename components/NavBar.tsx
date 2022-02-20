d229b9c04aebb3fef513e09bb6c0dce60a916461
soyalattee <uyyu1734@gmail.com>
2022-02-21 03:46:44 +0900

    stlye module 로 사용하기

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,17 +1,28 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-
+import styles from "./NavBar.module.css";
 export default function NavBar() {
   //loation에 관한 정보를 얻을 수 있다.
   const router = useRouter();
   return (
-    <nav>
+    <nav className={styles.nav}>
       {/* using Link! 리로드되는걸 방지한다. style 혹은 className은 a Tag 에서 작업한다. */}
       <Link href="/">
-        <a style={{ color: router.pathname == "/" ? "red" : "blue" }}>Home</a>
+        <a
+          className={[
+            styles.links,
+            router.pathname === "/" ? styles.active : "",
+          ].join(" ")}
+        >
+          Home
+        </a>
       </Link>
       <Link href="/about">
-        <a style={{ color: router.pathname == "/about" ? "red" : "blue" }}>
+        <a
+          className={`${styles.link}  ${
+            router.pathname === "/about" ? styles.active : ""
+          }`}
+        >
           About
         </a>
       </Link>
